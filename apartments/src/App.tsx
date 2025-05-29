4da9ab963d9a441f2d1b51c6a91f1525e0e8d839
Mai Thanh Hương <maithanhhuong2207@gmail.com>
2025-05-29 14:37:31 +0700

    fe

--- apartments/src/App.tsx
+++ apartments/src/App.tsx
@@ -23,9 +23,8 @@ import SettingsPage from "@/pages/Settings";
 // import ResidentNotifications from "./pages/resident/Notifications.tsx";
 // import ResidentFeedback from "./pages/resident/Feedback.tsx";
 
-
 //Accountant Page
-import Accountant from "./pages/Billing.tsx"; 
+import Accountant from "./pages/Billing.tsx";
 
 const queryClient = new QueryClient();
 
@@ -43,13 +42,10 @@ const App = () => (
           <Route path="/residents" element={<Residents />} />
           <Route path="/service-requests" element={<ServiceRequests />} />
           <Route path="/billing" element={<Billing />} />
-
+          <Route path="/settings" element={<SettingsPage />} />
 
           {/* Accountant Routes */}
           <Route path="/accountant" element={<Accountant />} />
-
-        
-
 
           {/* Catch-all route */}
           <Route path="*" element={<NotFound />} />
