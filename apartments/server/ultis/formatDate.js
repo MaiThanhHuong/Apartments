9139e8ae613ba49ad126f72da62d3320d9c030c8
PhanThanhThang04 <phanthanhthangdtht123@gmail.com>
2025-06-04 10:52:16 +0700

    fix down pdf

--- apartments/server/ultis/formatDate.js
+++ apartments/server/ultis/formatDate.js
@@ -1,7 +1,13 @@
-function formatDate(dateStr) {
+export function formatDatever1(dateStr) {
   if (!dateStr) return '';
   const d = new Date(dateStr);
   return d.toLocaleDateString('vi-VN');
 }
 
-export default formatDate;
+export function formatDatever2(dateStr) {
+  const d = new Date(dateStr);
+  const day = String(d.getDate()).padStart(2, '0');
+  const month = String(d.getMonth() + 1).padStart(2, '0');
+  const year = d.getFullYear();
+  return `${day}/${month}/${year}`;
+}
